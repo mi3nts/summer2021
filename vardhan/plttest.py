30229ba2ea04e3a19bf9bf272d2b5c0092bf2993
VA_24 <60634457+VA-24@users.noreply.github.com>
2021-06-03 10:45:17 -0500

    Add files via upload

---
+++ vardhan/plttest.py
@@ -0,0 +1,29 @@
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+x_vals = []
+y_vals = []
+
+index = count()
+
+
+def animate(i):
+    data = pd.read_csv('data.csv')
+    x = data['x_value']
+    y = data['speed_data']
+
+    plt.cla()
+
+    plt.plot(x, y)
+
+    plt.tight_layout()
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+plt.tight_layout()
+plt.show()
