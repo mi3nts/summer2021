85cda66b3ab70341d256f5ca68b7dc776445a978
VA_24 <60634457+VA-24@users.noreply.github.com>
2021-06-03 10:44:43 -0500

    Update subscribe.py

--- vardhan/subscribe.py
+++ vardhan/subscribe.py
@@ -1,10 +1,14 @@
 import paho.mqtt.client as mqtt
 import time
+import csv
 import matplotlib.pyplot as plt
 
+#initialize lists
 speed_list_1 = []
+fieldnames = ["x_value", "speed_data"]
 
-def on_message(client, userdata, message):
+
+def on_message(client, userdata, message, x_value = 1):
     print("received message: " ,str(message.payload.decode("utf-8")))
     speed_list_1.append(message.payload.decode("utf-8"))
 
@@ -14,23 +18,50 @@ mqttBroker ="mqtt.eclipseprojects.io"
 client = mqtt.Client("Database")
 client.connect(mqttBroker)
 
-client.loop_start()
+x_value = 0
 
-client.subscribe("Speed")
-client.on_message=on_message
+#infinite loop that continuously sends data to csv
+while True:
+    client.loop_start()
 
-time.sleep(30)
-client.loop_stop()
-
-#matplotlib code
-numbers = []
-speed_list = [float(x) for x in speed_list_1]
+    client.subscribe("Speed")
+    client.on_message=on_message
 
 
-for i in range (len(speed_list_1)):
-    numbers.append(i)
-plt.plot(numbers, speed_list)
-plt.title("Speed Vs. Time")
-plt.xlabel("Time")
-plt.ylabel("Speed")
-plt.show()
+    with open('data.csv', 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        if (len(speed_list_1) == 0):
+            info = {
+                "x_value": x_value,
+                "speed_data": 0,
+            }
+        else:
+            info = {
+                "x_value": x_value,
+                "speed_data": speed_list_1[-1],
+            }
+        csv_writer.writerow(info)
+        x_value += 1
+
+
+
+    time.sleep(1)
+    client.loop_stop()
+
+
+#print(speed_list_1)
+
+
+#out with the old, in with the new
+
+# numbers = []
+# speed_list = [float(x) for x in speed_list_1]
+# 
+# 
+# for i in range (len(speed_list_1)):
+#     numbers.append(i)
+# plt.plot(numbers, speed_list)
+# plt.title("Speed Vs. Time")
+# plt.xlabel("Time")
+# plt.ylabel("Speed")
+# plt.show()
