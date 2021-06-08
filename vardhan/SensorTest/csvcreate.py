956806fd5dca533294ef95267d2a349b70fd43b1
VA_24 <60634457+VA-24@users.noreply.github.com>
2021-06-08 16:20:30 -0500

    Update csvcreate.py

--- vardhan/SensorTest/csvcreate.py
+++ vardhan/SensorTest/csvcreate.py
@@ -1,11 +1,13 @@
 import csv
+from getmac import get_mac_address as gma
 import random
 import time
 
+macaddr = gma()
+mac_csv = macaddr.replace(":", "")
+
 fieldnames = ["dateTime", "Temperature", "Pressure", "Humidity", "Altitude"]
 
-
-with open('4c1d96a4f70e_BME20.csv', 'w') as csv_file:
+with open(f'{mac_csv}_BME20.csv', 'w') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     csv_writer.writeheader()
-
