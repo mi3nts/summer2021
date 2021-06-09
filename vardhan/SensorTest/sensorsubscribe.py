f829aaad087f590a3569006a4ad5bff0794f5aed
VA_24 <60634457+VA-24@users.noreply.github.com>
2021-06-09 15:16:25 -0500

    Update sensorsubscribe.py

--- vardhan/SensorTest/sensorsubscribe.py
+++ vardhan/SensorTest/sensorsubscribe.py
@@ -4,33 +4,40 @@ import time
 import csv
 import datetime
 import json
+import collections
 
 macaddr = gma()
-mac_csv = macaddr.replace(":", "")
-
-#in actual implementation, all of the mac addresses here can be read in through f strings, not just copy and pasting
-#initialize lists
-Temperature = []
-Pressure = []
-Humidity = []
-Altitude = []
-fieldnames = ["dateTime", "Temperature", "Pressure", "Humidity", "Altitude"]
+mac_name = macaddr.replace(":", "")
+weathersensor_id = "BME20"
+rainsensor_id = "GCCG41"
 
 
 def on_message(client, userdata, message):
-    current_time = datetime.datetime.now()
-    if message.topic == f"{macaddr}/Temperature":
-        print("Received message: " + str(message.payload.decode("utf-8")) + " at time " + str(current_time))
-        Temperature.append(message.payload.decode("utf-8"))
-    if message.topic == f"{macaddr}/Pressure":
-        print("Received message: " + str(message.payload.decode("utf-8")) + " at time " + str(current_time))
-        Pressure.append(message.payload.decode("utf-8"))
-    if message.topic == f"{macaddr}/Humidity":
-        print("Received message: " + str(message.payload.decode("utf-8")) + " at time " + str(current_time))
-        Humidity.append(message.payload.decode("utf-8"))
-    if message.topic == f"{macaddr}/Altitude":
-        print("Received message: " + str(message.payload.decode("utf-8")) + " at time " + str(current_time))
-        Altitude.append(message.payload.decode("utf-8"))
+    decoder = json.JSONDecoder(object_pairs_hook=collections.OrderedDict)
+    if message.topic == "4c1d96a4f70e/BME20":
+        weathersensorDict = decoder.decode(message.payload.decode("utf-8","ignore"))
+        print(str(weathersensorDict))
+        with open(f"{mac_name}_{weathersensor_id}.json", "w") as fp:
+            json.dump(message.payload.decode("utf-8"), fp)
+        weather = []
+        for key, value in weathersensorDict.items():
+            weather.append(value)
+        with open(f"{mac_name}_{weathersensor_id}.csv", "a") as outfile:
+            csvwriter = csv.writer(outfile)
+            csvwriter.writerow(weather)
+        print("-"*20)
+    elif message.topic == "4c1d96a4f70e/GCCG41":
+        rainsensorDict = decoder.decode(message.payload.decode("utf-8", "ignore"))
+        print(str(rainsensorDict))
+        with open(f"{mac_name}_{rainsensor_id}.json", "w") as fp:
+            json.dump(message.payload.decode("utf-8"), fp)
+        rain = []
+        for key, value in rainsensorDict.items():
+            rain.append(value)
+        with open(f"{mac_name}_{rainsensor_id}.csv", "a") as outfile:
+            csvwriter = csv.writer(outfile)
+            csvwriter.writerow(rain)
+
 
 #random broker hosting
 mqttBroker ="mqtt.eclipseprojects.io"
@@ -38,59 +45,33 @@ mqttBroker ="mqtt.eclipseprojects.io"
 client = mqtt.Client("Database")
 client.connect(mqttBroker)
 
-#x_value = 0
 
 #infinite loop that continuously sends data to csv
 while True:
     client.loop_start()
-
-    client.subscribe(f"{macaddr}/Temperature")
-    client.subscribe(f"{macaddr}/Pressure")
-    client.subscribe(f"{macaddr}/Humidity")
-    client.subscribe(f"{macaddr}/Altitude")
-
+    client.subscribe(f"{mac_name}/{weathersensor_id}")
+    client.subscribe(f"{mac_name}/{rainsensor_id}")
     client.on_message = on_message
 
-
-    with open(f"{mac_csv}_BME20.csv", 'a') as csv_file:
-        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        if (len(Altitude) == 0):
-            info = {
-                "dateTime": 0,
-                "Temperature": 0,
-                "Pressure" : 0,
-                "Humidity" : 0,
-                "Altitude" :0,
-            }
-        else:
-            info = {
-                "dateTime": str(datetime.datetime.now()),
-                "Temperature": Temperature[-1],
-                "Pressure": Pressure[-1],
-                "Humidity": Humidity[-1],
-                "Altitude": Altitude[-1],
-            }
-        csv_writer.writerow(info)
-
-    data = {}
-
-    if (len(Altitude) == 0):
-        data["dateTime"] = 0
-        data[f"{macaddr}/Temperature"] = 0
-        data[f"{macaddr}/Pressure"] = 0
-        data[f"{macaddr}/Humidity"] = 0
-        data[f"{macaddr}/Altitude"] = 0
-    else:
-        data["dateTime"] = str(datetime.datetime.now())
-        data[f"{macaddr}/Temperature"] = Temperature[-1]
-        data[f"{macaddr}/Pressure"] = Pressure[-1]
-        data[f"{macaddr}/Humidity"] = Humidity[-1]
-        data[f"{macaddr}/Altitude"] = Altitude[-1]
-
-    #write most recent data into json file
-    with open("sensor.json", "w") as fp:
-        json.dump(data, fp)
-
+    # with open(f"{mac_name}_{weathersensor_id}.csv", 'a') as csv_file:
+    #     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    #     if (len(Altitude) == 0):
+    #         info = {
+    #             "dateTime": 0,
+    #             "Temperature": 0,
+    #             "Pressure" : 0,
+    #             "Humidity" : 0,
+    #             "Altitude" :0,
+    #         }
+    #     else:
+    #         info = {
+    #             "dateTime": str(datetime.datetime.now()),
+    #             "Temperature": Temperature[-1],
+    #             "Pressure": Pressure[-1],
+    #             "Humidity": Humidity[-1],
+    #             "Altitude": Altitude[-1],
+    #         }
+    #     csv_writer.writerow(info)
 
     time.sleep(1)
     client.loop_stop()
