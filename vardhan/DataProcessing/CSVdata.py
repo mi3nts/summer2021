1594326d0427ef997757dcbd2a56f9911d164e3f
VA_24 <60634457+VA-24@users.noreply.github.com>
2021-07-09 12:29:46 -0500

    Update CSVdata.py

--- vardhan/DataProcessing/CSVdata.py
+++ vardhan/DataProcessing/CSVdata.py
@@ -1,7 +1,37 @@
 import pandas as pd
 import csv
+import math
 
-utdNode = pd.read_csv("C:/Users/va648/PycharmProjects/mintsML/files/mintsUTDData.csv")
-EPAdata = pd.read_csv("C:/Users/va648/PycharmProjects/mintsML/files/EPAdata.csv")
-print(type(utdNode.dateTime[0]))
-print(type(EPAdata.dateTime[0]))
+utdNode = pd.read_csv("C:/Users/usr/PycharmProjects/mintsML/files/mintsUTDData.csv")
+EPAdata = pd.read_csv("C:/Users/usr/PycharmProjects/mintsML/files/EPAdata.csv")
+
+
+measureEPAList = EPAdata.measurement.tolist()
+measureNodeList = utdNode.OPCN2_pm2_5.tolist()
+measureEPAAvg = []
+measureNodeAvg = []
+
+sum = 0
+
+#for EPA list
+# for i in range(math.floor(len(measureEPAList)/60) - 1):
+
+#     for j in range(i*60, ((i+1)*60)-1):
+#         sum = sum + measureEPAList[j]
+#         avg = sum/60
+
+#     measureEPAAvg.append(avg)
+#     sum = 0
+# print(measureAvg)
+
+#for Node list
+
+for i in range(math.floor(len(measureNodeList)/60) - 1):
+
+    for j in range(i*60, ((i+1)*60)-1):
+        sum = sum + measureNodeList[j]
+        avg = sum/60
+
+    measureNodeAvg.append(avg)
+    sum = 0
+print(measureNodeAvg)
