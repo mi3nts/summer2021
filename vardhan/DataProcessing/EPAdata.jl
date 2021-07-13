1aaa3e69ba1a43f0299629434042c6170a98f7a8
VA_24 <60634457+VA-24@users.noreply.github.com>
2021-07-13 17:36:57 -0500

    Update EPAdata.jl

--- vardhan/DataProcessing/EPAdata.jl
+++ vardhan/DataProcessing/EPAdata.jl
@@ -21,17 +21,25 @@ import Dates
 % # ***********************************************************************
 % #   DataFrames concatenation
 
+#simple function to edit the url
+url1 = ""
+function get2020()
+    url1 = "https://aqs.epa.gov/data/api/sampleData/bySite?email=va648314@gmail.com&key=bayhare31&param=88101&bdate=20200101&edate=20201231&state=48&county=439&site=1006"
+    return url1
+end
 
-
-resp = HTTP.get("https://aqs.epa.gov/data/api/sampleData/bySite?email=va648314@gmail.com&key=bayhare31&param=88101&bdate=20200101&edate=20201231&state=48&county=439&site=1006")
+resp = HTTP.get(get2020())
 str = String(resp.body)
 jobj = JSON.parse(str)
 
 dateTimeList = []
 latList = []
 longList = []
-measureList = []
+measureEPAList = []
 
+
+dfUTD = DataFrame(CSV.File("C:/Users/va648/PycharmProjects/mintsML/files/UTD2_5Data.csv"))
+measureUTDList = dfUTD.OPCN2_pm2_5
 
 for i in 1:length(jobj["Data"])
     dateStr = jobj["Data"][i]["date_local"]
@@ -40,38 +48,35 @@ for i in 1:length(jobj["Data"])
     longitude = jobj["Data"][i]["longitude"]
     measurement = jobj["Data"][i]["sample_measurement"]
 
-    dateSplit = split(dateStr, "-")
-    timeSplit = time[1:2]
-
-    dateCompile = dateSplit[1] * dateSplit[2] * dateSplit[3]
-    timeCompile = timeSplit * "0000"
-
-    dateTime = dateCompile * " " * timeCompile
-
-
-    df = Dates.DateTime(dateTime, "yyyymmdd HHMMSS")
-
-#     year = parse(Int64, dateStr[1:4])
-#     month = parse(Int64, dateStr[6:7])
-#     date = parse(Int64, dateStr[9:10])
+#     dateSplit = split(dateStr, "-")
+#     timeSplit = time[1:2]
 #
-#     dateDateType = Dates.Date(year, month, date)
+#     dateCompile = dateSplit[1] * dateSplit[2] * dateSplit[3]
+#     timeCompile = timeSplit * "0000"
+#
+#     dateTime = dateCompile * " " * timeCompile
+#
+#
+#     df = Dates.DateTime(dateTime, "yyyymmdd HHMMSS")
+
+    dateTime = dateStr * " " * time
 
     if measurement == nothing
         continue
     else
-        push!(dateTimeList, df)
+        push!(dateTimeList, dateTime)
         push!(latList, latitude)
         push!(longList, longitude)
-        push!(measureList, measurement)
+        push!(measureEPAList, measurement)
     end
 end
 
 reverseDateTime = reverse(dateTimeList)
 reverseLat = reverse(latList)
 reverseLong = reverse(longList)
-reverseMeasure = reverse(measureList)
+reverseEPAMeasure = reverse(measureEPAList)
+reverseUTDMeasure = reverse(measureUTDList)
 
-df = DataFrame(dateTime = reverseDateTime, latitude = reverseLat, longitude = reverseLong, measurement = reverseMeasure )
+df = DataFrame(dateTime = reverseDateTime, latitude = reverseLat, longitude = reverseLong, measurementEPA = reverseEPAMeasure)
 println(df)
-CSV.write("files/EPAdata.csv", df)
+# CSV.write("files/EPAdata.csv", df)
